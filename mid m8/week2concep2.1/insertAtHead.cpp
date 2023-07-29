05ff234f4cb9ca783a921a8e056648f918bdb937
Rabiul Islam <rabiulislamdv@gmail.com>
2023-07-29 06:42:36 +0600

    insertAtAnyPosition

--- mid m8/week2concep2.1/insertAtHead.cpp
+++ mid m8/week2concep2.1/insertAtHead.cpp
@@ -12,6 +12,17 @@ class Node
     }
 };
 
+// seze count korte
+int getSize( Node* head){
+    Node* temp = head;
+    int count = 0;
+    while(temp != NULL)
+    {
+        temp = temp->next;
+        count++;
+    }
+    return count ;
+}
 void insertAtTail( Node * &head, int val){
     Node * newNode = new Node(val);
     if( head == NULL)
@@ -26,15 +37,8 @@ void insertAtTail( Node * &head, int val){
     }
     temp->next = newNode;
 }
-void printList(Node * head)
-{
-    Node * temp = head;
-    while(temp !=NULL)
-    {
-        cout<< temp->val<< " ";
-        temp= temp->next;
-    }
-}
+
+
 // head a insert korte
 void insertAtHead( Node * &head, int val)
 {
@@ -48,6 +52,39 @@ void insertAtHead( Node * &head, int val)
     head = newNode;
     newNode->next = temp;
 }
+    void insertAtAnyPosition(Node * & head, int index,int val){
+    Node * newNode = new Node(val);
+    if(index == 1)
+    {
+        insertAtHead(head,val);
+        return ;
+    } 
+    else if(index==getSize(head)){
+        insertAtTail(head,val);
+        return ;
+    }
+    Node * temp = head;
+    int pos = 1;
+
+    while(pos !=index-1){
+        temp=temp->next;
+        pos++;
+        
+    }
+    Node* temp2 = temp->next;
+    temp->next = newNode;
+    newNode->next = temp2;
+}
+void printList(Node * head)
+{
+    Node * temp = head;
+    while(temp !=NULL)
+    {
+        cout<< temp->val<< " ";
+        temp= temp->next;
+    }
+    cout<< endl;
+}
 int main()
 {
 
@@ -56,9 +93,17 @@ int main()
     insertAtTail(head,20);
     insertAtTail(head,30);
     insertAtTail(head,40);
-
+ 
     insertAtHead(head,60);
     insertAtHead(head,70);
+
+    printList(head);
+
+    // size pete
+    cout<<getSize(head) <<endl;
+    insertAtAnyPosition(head, 1,80);
+    insertAtAnyPosition(head, 7,80);
+    insertAtAnyPosition(head, 3,8520);
 
     printList(head);
     return 0;
