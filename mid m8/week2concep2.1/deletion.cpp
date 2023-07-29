0cc613aa3fc80c7a380b6e2860dbf01f7ca9b75a
Rabiul Islam <rabiulislamdv@gmail.com>
2023-07-29 07:06:06 +0600

    deleteFormHeadTail

---
+++ mid m8/week2concep2.1/deletion.cpp
@@ -0,0 +1,122 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node
+{
+    public:
+    int val; 
+    Node* next;
+    Node(int val)
+    {
+        this->val=val;
+        this->next=NULL;
+    }
+};
+
+// seze count korte
+int getSize( Node* head){
+    Node* temp = head;
+    int count = 0;
+    while(temp != NULL)
+    {
+        temp = temp->next;
+        count++;
+    }
+    return count ;
+}
+void insertAtTail( Node * &head, int val){
+    Node * newNode = new Node(val);
+    if( head == NULL)
+    {
+        head = newNode;
+        return ;
+    }
+    Node * temp = head;
+    while(temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+
+// head a insert korte
+void insertAtHead( Node * &head, int val)
+{
+    Node* newNode = new Node(val);
+    if( head == NULL)
+    {
+         head = newNode;
+         return;
+    }
+    Node * temp = head;
+    head = newNode;
+    newNode->next = temp;
+}
+    void insertAtAnyPosition(Node * & head, int index,int val){
+    Node * newNode = new Node(val);
+    if(index == 1)
+    {
+        insertAtHead(head,val);
+        return ;
+    } 
+    else if(index==getSize(head)){
+        insertAtTail(head,val);
+        return ;
+    }
+    Node * temp = head;
+    int pos = 1;
+
+    while(pos !=index-1){
+        temp=temp->next;
+        pos++;
+        
+    }
+    Node* temp2 = temp->next;
+    temp->next = newNode;
+    newNode->next = temp2;
+}
+// delete at head
+void deleteFormHead(Node * &head){
+    Node* temp = head;
+
+    Node * temp2 = temp->next;
+    head = temp2;
+    delete temp;
+
+}
+//deleteFormTail
+void deleteFormTail(Node * &head){
+    Node * temp = head;
+    while(temp->next->next !=NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = NULL;
+}
+void printList(Node * head)
+{
+    Node * temp = head;
+    while(temp !=NULL)
+    {
+        cout<< temp->val<< " ";
+        temp= temp->next;
+    }
+    cout<< endl;
+}
+
+int main()
+{
+
+    Node * head = NULL;
+    insertAtTail(head,10);
+    insertAtTail(head,20);
+    insertAtTail(head,30);
+    insertAtTail(head,40);
+
+    deleteFormHead(head);
+    deleteFormTail(head);
+    printList(head);
+
+
+    return 0;
+}
